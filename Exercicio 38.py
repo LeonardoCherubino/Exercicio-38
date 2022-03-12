1dbf1c9253b8d6714290234027ee2bfef886993d
Leonardo Cherubino <96987724+LeonardoCherubino@users.noreply.github.com>
2022-03-12 09:35:45 -0300

    first commit

---
+++ Exercicio 38.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e imprima na tela o resultado em milímetros
+
+metros = float(input('Entre com o valor em metros: '))
+milimetros = metros * 1000
+print('{}m correspondem a {}mm'.format(metros, milimetros))
